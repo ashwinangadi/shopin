b3e4c7cf026e79aef53d56c8a5ed62bd5a828d21
Ashwin <ashwin.angadi1@gmail.com>
2024-10-07 13:04:11 +0530

    Fix: Dynamic code evaluation while build

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,12 @@
 export { auth as middleware } from "../auth";
 export const config = {
+  runtime: "nodejs",
+  unstable_allowDynamic: [
+    "/node_modules/mongoose/**",
+    "mongoose/dist/browser.umd.js",
+    "/src/models/userModel.ts",
+    "/src/lib/db.ts",
+    "/src/lib/actions.ts",
+  ],
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
 };
