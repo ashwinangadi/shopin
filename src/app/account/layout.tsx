05858acab1791f68f02b32f1c12f73cf89da9aeb
Ashwin <ashwin.angadi1@gmail.com>
2024-10-09 09:39:37 +0530

    updated routes and layout for account

---
+++ src/app/account/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ShopIN | Account",
+  description: "Shopping Application built on Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  //   const queryClient = getQueryClient();
+
+  //   // queryClient.prefetchQuery(dealsOptions);
+  //   queryClient.prefetchQuery(categoryListOptions);
+  //   connectToMongoDB()
+  return (
+    <section className=" min-h-[calc(100vh-96px)] md:min-h-[calc(100vh-64px)] mt-24 md:mt-16">
+      {children}
+    </section>
+  );
+}
