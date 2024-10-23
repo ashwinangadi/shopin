70edbde074b58f736d4c22275798a6e14ead8185
Ashwin <ashwin.angadi1@gmail.com>
2024-10-23 23:40:45 +0530

    added TODO for account

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 const AccountPage = () => {
   return (
+    // TODO: Add User name in the session data of credentials, Only getting user name from the Google and Github providers.
     <div className="flex flex-col gap-2 ">
       <h1 className="text-xl font-medium py-3 px-5">My Account</h1>
       <SelectSeparator className="w-full" />
